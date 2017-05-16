9300729c88cdb1d32f2c8561d8fb08ae3b7b4021
jato333 <yubodou333@126.com>
2017-05-16 10:41:38 +0800

    change

---
+++ src/main/java/com/renyuwo/alamp/Application.java
@@ -0,0 +1,25 @@
+package com.renyuwo.alamp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+@MapperScan("com.*.mapper")
+@ComponentScan(basePackages={"com.renyuwo"})
+public class Application extends SpringBootServletInitializer{
+	@Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
