bfeca7f099f6f0b4fc56f05109f3179944d209d2
jato333 <yubodou333@126.com>
2017-05-17 10:27:55 +0800

    add ScheduledTask

--- src/main/java/com/renyuwo/alamp/task/ScheduledTasks.java
+++ src/main/java/com/renyuwo/alamp/task/ScheduledTasks.java
@@ -3,7 +3,9 @@ package com.renyuwo.alamp.task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ScheduledTasks {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
